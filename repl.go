3eae16d48195903342f882e4019c4456be6320fb
Hamad <nhamad919@gmail.com>
2026-06-28 09:53:48 +0400

    repl functionality and clean input

---
+++ repl.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Println("Your command was: " + words[0])
+	}
+}
