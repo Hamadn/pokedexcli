31bb4035d329ed05b8c658aae175f5b15e9cfa22
Hamad <nhamad919@gmail.com>
2026-06-28 10:29:57 +0400

    added cli commands for help and exit

--- repl.go
+++ repl.go
@@ -7,10 +7,48 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name     string
+	desc     string
+	callback func() error
+}
+
+func commands() map[string]cliCommand {
+	commands := map[string]cliCommand{
+		"exit": {
+			name:     "exit",
+			desc:     "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name:     "help",
+			desc:     "Prints help",
+			callback: commandHelp,
+		},
+	}
+	return commands
+}
+
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Print("Usage:\n\n")
+
+	for _, cmd := range commands() {
+		fmt.Println(cmd.name, ":", cmd.desc)
+	}
+	return nil
 }
 
 func startRepl() {
@@ -22,6 +60,13 @@ func startRepl() {
 		if len(words) == 0 {
 			continue
 		}
-		fmt.Println("Your command was: " + words[0])
+		switch cmd := words[0]; cmd {
+		case "exit":
+			commandExit()
+		case "help":
+			commandHelp()
+		default:
+			fmt.Println("Unknown command")
+		}
 	}
 }
